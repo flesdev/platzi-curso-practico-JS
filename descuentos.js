eae4b239e4c77652290582409b859a3d7ad0b28c
Lester Inga <lester11inga@gmail.com>
2021-07-29 17:30:56 -0500

    cambios para el segundo taller

--- descuentos.js
+++ descuentos.js
@@ -8,3 +8,16 @@ function calcularPrecioConDescuento(precio, descuento) {
 }
 
 console.log('');
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById('inputPrice');
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById('inputDiscount');
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultBox = document.getElementById('resultBox');
+    resultBox.innerText = ' precio con descuento es $' + precioConDescuento;
+}
