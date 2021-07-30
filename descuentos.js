403c6cb66f90cb4754a6831052355532f39159a0
Lester Inga <lester11inga@gmail.com>
2021-07-30 14:25:42 -0500

    finalizacion del taller 2

--- descuentos.js
+++ descuentos.js
@@ -14,8 +14,31 @@ function onClickButtonPriceDiscount() {
     const inputDiscount = document.getElementById('inputDiscount');
     const discountValue = inputDiscount.value;
 
+    const inputCoupon = document.getElementById('inputDiscountCoupons');
+    const couponValue = inputCoupon.value;
+
+    let coupon;
+
+    if (couponValue == 'platzi') {
+        coupon = coupons[0];
+    } else if (couponValue == 'expert') {
+        coupon = coupons[1];
+    } else if (couponValue == 'basic') {
+        coupon = coupons[2];
+    } else if (couponValue == 'expert+') {
+        coupon = coupons[3];
+    } else {
+        coupon = 0;
+    }
+
     const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
 
+    const precioConDescuentoCoupon = calcularPrecioConDescuento(precioConDescuento, coupon);
+
     const resultBox = document.getElementById('resultBox');
-    resultBox.innerText = 'El precio con descuento es $' + precioConDescuento + '.';
+    resultBox.innerText = 'El precio con descuento es: $ ' + precioConDescuentoCoupon + '.';
 }
+
+/* ============= Cupones de Descuento ============= */
+
+const coupons =[ 8, 10, 5, 12];
